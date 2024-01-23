f180173f853d4b130413c3eba2fefcfd54e6b597
logapathan <75413426+logapathan@users.noreply.github.com>
2024-01-23 17:49:01 +0530

    Add files via upload

---
+++ studentdata_using_functions.py
@@ -0,0 +1,8 @@
+def studentdata(name,id,age):
+    print(f"Student name is {name}")
+    print(f"Student id is {id}")
+    print(f"Student age is {age}")
+name=input("Enter student name:")
+id=int(input("Enter student id number:"))
+age=int(input("Enter student age:"))
+studentdata(name,id,age)
